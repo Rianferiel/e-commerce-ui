2bd55789bbb8766951e1dad12ef5447b2c985a75
Rianferiel <rianferiel@gmail.com>
2026-01-04 16:07:21 +0700

    create-product-card-component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,12 +1,103 @@
-"use client"
+"use client";
 
-import { ProductType } from '@/types'
-import React from 'react'
+import { ProductType } from "@/types";
+import { ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
 
-const ProductCard = ({product}:{product:ProductType}) => {
+const ProductCard = ({ product }: { product: ProductType }) => {
+  const [productTypes, setProductTypes] = useState({
+    size: product.sizes[0],
+    color: product.colors[0],
+  });
+
+  const handleProductType = ({
+    type,
+    value,
+  }: {
+    type: "size" | "color";
+    value: string;
+  }) => {
+    setProductTypes((prev) => ({
+      ...prev,
+      [type]: value,
+    }));
+  };
   return (
-    <div>ProductCard</div>
-  )
-}
+    <div className="shadow-lg rounded-lg overflow-hidden">
+      <Link href={`/products/${product.id}`}>
+        <div className="relative aspect-[2/3]">
+          <Image
+            src={product.images[productTypes.color]}
+            alt={product.name}
+            fill
+            className="object-cover hover:scale-105 transition-all duration-300"
+          />
+        </div>
+      </Link>
+      {/* DETAIL */}
+      <div className="flex flex-col gap-4 p-4">
+        <h1 className="font-medium">{product.name}</h1>
+        <p className="text-sm text-gray-500">{product.description}</p>
+        {/* TYPES */}
+        <div className="flex items-center gap-4 text-xs">
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Size</span>
+            <select
+              name="size"
+              id="size"
+              className="ring ring-gray-300 rounded-md px-2 py-1"
+              onChange={(e) =>
+                handleProductType({ type: "size", value: e.target.value })
+              }
+            >
+              {product.sizes.map((size) => (
+                <option value={size} key={size}>
+                  {size.toUpperCase()}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="flex flex-col gap-1">
+            <span className="text-gray-500">Colors</span>
+            <div className="flex items-center gap-2">
+              {product.colors.map((color) => (
+                <div
+                  className={`cursor-pointer border-1 ${
+                    productTypes.color === color
+                      ? "border-gray-400"
+                      : "border-gray-200"
+                  } rounded-full p-[1.5px]`}
+                  key={color}
+                  onClick={() =>
+                    handleProductType({ type: "color", value: color })
+                  }
+                >
+                  <div
+                    className="w-[14px] h-[14px] rounded-full"
+                    style={{ backgroundColor: color }}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        {/* PRICE AND ADD TO CART BUTTON */}
+        <div className="flex items-center justify-between gap-4">
+          <div className="text-md">
+            <p>${product.price.toFixed(2)}</p>
+          </div>
+          <div className="text-sm">
+            <button className="flex items-center gap-2 ring ring-gray-200 px-2 py-1 rounded-md shadow-md cursor-pointer hover:text-white hover:bg-black transition-all duration-300">
+              <ShoppingCart size={16} />
+              <p>Add to Cart</p>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
